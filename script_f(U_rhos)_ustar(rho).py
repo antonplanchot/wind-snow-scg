cfed35c2a6f50b02bebcaf7fd485f6a468421aa5
antonplanchot <105217966+antonplanchot@users.noreply.github.com>
2022-07-05 19:51:00 +0200

    Add files via upload

---
+++ script_f(U_rhos)_ustar(rho).py
@@ -0,0 +1,128 @@
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import axes3d  # Fonction pour la 3D
+import numpy as np
+import netCDF4 as nc
+
+data=nc.Dataset(r"C:\Users\Toshiba\Documents\stage L3\Documents\code_amory_5_juin\forcings_20100101-20200101.nc")
+
+#extraire les variables meteo du fichier netcdf
+U=data.variables['U2'][:,0,0]   #[m/s]
+
+##variables temporelles
+date=[]
+an=2010
+while an<2020:
+    a=0
+    if an==2012 or an==2016:
+        nb_jour=366
+    else:
+        nb_jour=365
+    while a<nb_jour*24:
+        date.append(an+a/(24*nb_jour))
+        a+=1
+    an+=1
+date.remove(2010)
+date.append(2020)
+
+##declaration des constantes
+rhos0=300
+rhoice=920
+
+vk=0.4   #constante de Von Karman   sans dimension
+
+d=0.5    #dendricite
+s=0.5   #sphericite
+
+C_D=10**-3   #coefficient de drag pour le moment   sans dimension
+z0=10**-4   #longueur de rugosite aerodynamique   m
+
+#calcule u* a partir de U
+ustar=U*vk/np.log(2/z0)
+
+#indice d'erodabilite
+iER=0.75*d-0.5*s+0.5
+
+#vitesse de friction seuil standard
+ustarT0=(C_D**(0.5))*(np.log(2.868)-np.log(1+iER))/0.085
+
+##plot 2D
+
+size=13
+parameters = {'axes.labelsize': size,'axes.titlesize': size,'legend.fontsize': size,'xtick.labelsize': size,'ytick.labelsize': size}
+plt.rcParams.update(parameters)
+
+x = np.arange(0,100, 1)
+y = np.arange(251,451,2)
+z=np.zeros((len(x),len(y)))
+for i in range(len(x)):
+    for j in range(len(y)):
+        z[i,j]=0.25*x[i]*vk/(np.log(2/z0))-ustarT0*np.exp(rhoice/rhos0-rhoice/y[j])
+
+alpha = [ '300', '350','400','450']
+alpha2 = ['0','5','10', '15','20']
+
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+cax = ax.imshow(z,origin='lower')
+
+
+plt.plot(x,[4*ustarT0*np.exp(rhoice/rhos0-rhoice/k)/(vk/np.log(2/z0)) for k in y], color='r', label='u*=u*t',linewidth=5)
+fig.colorbar(cax,label='u*-u*t [sans unité]')
+#plt.plot([24,24],[0,100],color='aqua',label='ρs=ρ0',linewidth=5)
+plt.plot([99,99],[0,100],color='aqua',label='ρs=ρmax',linewidth=5)
+ax.set_xticks(range(24,100,25))
+ax.set_yticks(range(0,100,20))
+ax.set_xticklabels(alpha)
+ax.set_yticklabels(alpha2)
+plt.xlabel('ρs [kg/m3]')
+plt.ylabel('U [m/s]')
+plt.text(44,55,'érosion',color="red",fontsize='13')
+plt.text(40,15,"pas d'érosion",color="red",size='13')
+plt.legend()
+plt.grid()
+plt.xlim(0,100)
+plt.ylim(0,100)
+plt.savefig(r"C:\Users\Toshiba\Documents\stage L3\Documents\figures\f(u_rhos)2D.png")
+plt.show()
+
+
+# plt.plot([ustarT0*np.exp(rhoice/rhos0-rhoice/k) for k in y]) #ustarT
+# plt.show()
+
+##ustarT(rhos) pour différents rhos0
+size=13
+parameters = {'axes.labelsize': size,'axes.titlesize': size,'legend.fontsize': size,'xtick.labelsize': size,'ytick.labelsize': size}
+plt.rcParams.update(parameters)
+
+plt.figure(figsize=(8,9))
+plt.subplot(2,1,1)
+Lrhos0=[150,175,200,225,250,300,350]
+for x in Lrhos0:
+    Lrho=[k for k in range(x,451)]
+    LustarT=[ustarT0*np.exp(rhoice/x-rhoice/y) for y in Lrho]
+    if x==300:
+        plt.plot(Lrho,LustarT,color='r')
+    else:
+        plt.plot(Lrho,LustarT,color='b')
+
+size2='12'
+plt.text(180, 2.5,"ρ0=150 kg/m3", color='b',fontsize=size2)
+plt.text(285, 2.5,"175 kg/m3", color='b',fontsize=size2)
+plt.text(345, 2,"200 kg/m3", color='b',fontsize=size2)
+plt.text(385, 1.5,"225 kg/m3", color='b',fontsize=size2)
+plt.text(385, 1,"250 kg/m3", color='b',fontsize=size2)
+plt.text(365, 0.5,"300 kg/m3", color='r',fontsize=size2)
+plt.text(415, 0.2,"350 kg/m3", color='b',fontsize=size2)
+
+plt.xlabel("ρs [kg/m3]")
+plt.ylabel("u*t [m/s]")
+plt.ylim(0,3)
+plt.grid()
+plt.subplot(2,1,2)
+plt.plot(date[:40000],ustar[:40000])
+plt.xlabel('temps [ans)]')
+plt.ylabel('u*t [m/s]')
+plt.grid()
+plt.savefig(r"C:\Users\Toshiba\Documents\stage L3\Documents\figures\ustarT(rhos)_pour_differents_rhos0.png")
+plt.show()
